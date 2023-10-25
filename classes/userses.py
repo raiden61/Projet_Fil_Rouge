5f73f407fb2d4d3a09b382301a3f1c596438bf59
Raiden <adrien.graindorge@my-digital-school.org>
2023-10-25 17:17:35 +0200

    commit 25/10/2023

---
+++ classes/userses.py
@@ -0,0 +1,32 @@
+class Users:
+    def __init__(self, name, password, email, first_name, last_name):
+        self.id = None
+        self.username = name
+        self.password = password
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_map(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'mail': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name
+        }
+
+    @classmethod
+    def from_map(cls, map):
+        user = cls(
+        map.get('username', ''),
+        map.get('password', ''),
+        map.get('email', ''),
+        map.get('first_name', ''),
+        map.get('last_name', '')
+    )
+        user.id = map.get('id')
+
+
+        return user
--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from flask import Flask, request, jsonify # pip install flask
 import mysql.connector # pip install mysql-connector-python
 from controllers.univers import Univers_Controller
 from controllers.personnages import Personnages_Controller
+from controllers.users import users_Controller
 
 # Création de l'application Flask
 app = Flask(__name__)
 class Test(Univers_Controller):
+    # Définition des routes pour les users
+    @app.route('/users', methods=['GET', 'POST'])
+    def handle_users():
+        return users_Controller.user_Method()
+    @app.route('/users/<string:users>', methods=['GET', 'PUT'])
+    def handle_usersSpecifique(users):
+        return users_Controller.user_MethodSpecifique(users)
 
     # Définition des routes pour les univers
     @app.route('/univers', methods=['GET', 'POST'])
@@ -16,6 +24,7 @@ class Test(Univers_Controller):
     def handle_universSpecifique(univers):
         return Univers_Controller.universMethodSpecifique(univers)
     
+    # Définition des routes pour les personnages
     @app.route('/univers/<string:univers>/personnages', methods=['GET', 'POST', 'PUT', 'DELETE'])
     def handle_personnages(univers):
         return Personnages_Controller.PersonnagesMethod(univers)
